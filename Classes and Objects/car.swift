196ea1cddc96abf40edfe45063847ec188c5fa8f
Kashif Ali <kashifali874@gmail.com>
2018-10-26 22:03:19 -0700

    Initial files of classes and images

---
+++ Classes and Objects/car.swift
@@ -0,0 +1,9 @@
+//
+//  car.swift
+//  Classes and Objects
+//
+//  Created by Ali on 10/26/18.
+//  Copyright © 2018 Kashif Ali. All rights reserved.
+//
+
+import Foundation
---
+++ Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Ali on 10/26/18.
+//  Copyright © 2018 Kashif Ali. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
